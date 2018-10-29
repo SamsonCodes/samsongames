76e7abd43e84e281282ee4ffb141cd3d953c4d5c
Samson <44621365+SamsonCodes@users.noreply.github.com>
2018-10-29 12:30:34 +0100

    Finished XMLReaderTest

--- test/customgame/tiled/XMLReaderTest.java
+++ test/customgame/tiled/XMLReaderTest.java
@@ -5,6 +5,7 @@
  */
 package customgame.tiled;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -15,7 +16,8 @@ import org.junit.Before;
  */
 public class XMLReaderTest {
     
-    private String source, expected;
+    private String source1, source2;
+    private ArrayList<String> list;
     
     public XMLReaderTest() {
     }
@@ -23,8 +25,11 @@ public class XMLReaderTest {
     @Before
     public void setUp()
     {
-         source = "blblblal<><>Aadjfa <info company=\"Toshiba\" date=\"25-10-19\"> adfa<Daaf> Daafafd><Ad";
-         expected = "<info company=\"Toshiba\" date=\"25-10-19\">";
+         source1 = "blblblal<><>Aadjfa <info company=\"Toshiba\" date=\"25-10-19\"> adfa<Daaf> Daafafd><Ad";
+         source2 = "<><Asda><thing> ahahaha, hadfhafhd </thing>da<a>33</a><thing attr=\"12\">dafda7777</thing> stuff";
+         list = new ArrayList();
+         list.add(source1);
+         list.add(source2);
          System.out.println("Set up complete!");
     }
 
@@ -32,7 +37,12 @@ public class XMLReaderTest {
      * Test of getElementsPlus method, of class XMLReader.
      */
     @Test
-    public void testGetElementsPlus_String_ArrayList() {        
+    public void testGetElementsPlus_String_ArrayList() {   
+        ArrayList<String> expected = new ArrayList();
+        expected.add("<thing> ahahaha, hadfhafhd </thing>");
+        expected.add("<thing attr=\"12\">dafda7777</thing>");
+        ArrayList<String> output = XMLReader.getElementsPlus("thing", list);
+        assertEquals(expected, output);
     }
 
     /**
@@ -40,8 +50,8 @@ public class XMLReaderTest {
      */
     @Test
     public void testGetElementPlus_String_String() {
-       
-        String output = XMLReader.getElementPlus("info", source);
+        String expected = "<info company=\"Toshiba\" date=\"25-10-19\">";
+        String output = XMLReader.getElementPlus("info", source1);
         assertEquals(expected, output);
     }
 
@@ -50,6 +60,10 @@ public class XMLReaderTest {
      */
     @Test
     public void testGetElementPlus_String_ArrayList() {
+        ArrayList<String> expected = new ArrayList();
+        expected.add("<info company=\"Toshiba\" date=\"25-10-19\">");
+        ArrayList<String> output = XMLReader.getElementsPlus("info", list);
+        assertEquals(expected, output);
     }
 
     /**
@@ -57,6 +71,14 @@ public class XMLReaderTest {
      */
     @Test
     public void testGetElementsPlus_String_String() {
+        String[] expected = {"<thing> ahahaha, hadfhafhd </thing>","<thing attr=\"12\">dafda7777</thing>"};
+        ArrayList<String> output = XMLReader.getElementsPlus("thing", source2);
+        String[] outputArray = new String[output.size()];
+        for(int i = 0; i < output.size(); i++)
+        {
+            outputArray[i] = output.get(i);
+        }
+        assertArrayEquals(expected, outputArray);
     }
 
     /**
@@ -64,6 +86,9 @@ public class XMLReaderTest {
      */
     @Test
     public void testGetAttribute() {
+        String expected = "12";
+        String output = XMLReader.getAttribute("attr", "<thing attr=\"12\">dafda7777</thing>");
+        assertEquals(expected, output);
     }
 
     /**
@@ -71,6 +96,9 @@ public class XMLReaderTest {
      */
     @Test
     public void testStripTags() {
+        String expected = "dafda7777";
+        String output = XMLReader.stripTags("thing", "<thing attr=\"12\">dafda7777</thing>");
+        assertEquals(expected, output);
     }
 
     /**
@@ -78,6 +106,17 @@ public class XMLReaderTest {
      */
     @Test
     public void testCountChar() {
+        String s1 = "1234567891";
+        String s2 = "kk ii kk 88";
+        int output1 = XMLReader.countChar('1', s1);
+        assertEquals(output1, 2);
+        int output2 = XMLReader.countChar('2', s1);
+        assertEquals(output2, 1);
+        int output3 = XMLReader.countChar(' ', s2);
+        assertEquals(output3, 3);
+        int output4 = XMLReader.countChar('k', s2);
+        assertEquals(output4, 4);
+        
     }
     
 }
